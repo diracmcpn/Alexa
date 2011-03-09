1553a2c6ddfa42b5ab6efa53529296b9aa481376
dirac <diracmcpn@hotmail.com>
2011-03-09 15:03:29 +0100

    Optimization of the code with regex

--- alexa.php
+++ alexa.php
@@ -6,25 +6,35 @@ $timeout is the maximum number of seconds to allow cURL functions to execute.
 
      function getAlexaRank($domain, $timeout = 10)
      {
-          //get the content of the webpage
-	  $handle = curl_init("http://www.alexa.com/search?q=".$domain."&r=site_screener&p=bigtop");
-	  curl_setopt($handle, CURLOPT_TIMEOUT, $timeout);
-	  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, $timeout);
-	  curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
-	  $data = curl_exec($handle);
-	  curl_close($handle);
-
-          //parse the webpage with DOM 
-	  $doc = new DOMDocument();
-	  $doc->loadHTML($data);
-	  $listul = $doc->getElementsByTagName('ul');
-	  foreach ($listul as $ul)
-	  {
-	       if ($ul->getAttribute("class")=="traffic-stats")
-	       {
-		   $rank = $ul->getElementsByTagName('li')->item(0)->getElementsByTagName('a')->item(0)->firstChild->nodeValue;
-	       }
-	  }
-	  return $rank;
+		//get the content of the webpage
+	  	$handle = curl_init("http://www.alexa.com/search?q=".$domain."&r=site_screener&p=bigtop");
+		curl_setopt($handle, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+		$data = curl_exec($handle);
+		curl_close($handle);
+		
+		//parse the webpage by regex 
+		preg_match("#Alexa Traffic Rank:(?:.+\s*){2}(.+)</a>#", $data, $result);
+		return intval(str_replace(',','', $result[1]));
      }
+
+?>
+
+<?php
+/*----TEST----*/
+
+//First example
+echo "Alexa Traffic Rank of google.com : ".getAlexaRank("google.com");
+
+//Second Example
+$domainArray = array("google.com"=>0, "hyip.com"=>0, "siteduzero.com"=>0, "yahoo.com"=>0);
+foreach($domainArray as $domain => $rank)
+{
+  $domainArray[$domain] = getAlexaRank($domain);
+}
+
+asort($domainArray);
+print_r($domainArray);
+
 ?>
